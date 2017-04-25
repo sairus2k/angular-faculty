cd9c87d84d311f031828a69795f890831a3a239a
Anton Savoskin <sairus2k@gmail.com>
2017-04-25 10:12:37 +0700

    Deploy to GitHub pages

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
 export default routesConfig;
 
 /** @ngInject */
-function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
-  $locationProvider.html5Mode(true).hashPrefix('!');
+function routesConfig($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
